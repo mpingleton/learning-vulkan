ca8f82d67c796e3c86710047a31aa495e6172dd2
Michael Pingleton <mnpingleton@gmail.com>
2023-10-04 18:52:22 -0400

    Added code that creates logical device.  Currently at page 18 in the red book.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,6 @@ int main(int argc, char** argv)
     // Create the instance.
     instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     instanceCreateInfo.pApplicationInfo = &appInfo;
-
     result = vkCreateInstance(&instanceCreateInfo, nullptr, &m_instance);
     if (result != VK_SUCCESS) return 1;
     
@@ -36,7 +35,7 @@ int main(int argc, char** argv)
     cout << "We have " << physicalDeviceCount << " devices on this system." << endl;
     m_physicalDevices.resize(physicalDeviceCount);
     vkEnumeratePhysicalDevices(m_instance, &physicalDeviceCount, m_physicalDevices.data());
-
+    /*
     for (int deviceIndex = 0;  deviceIndex < m_physicalDevices.size(); deviceIndex++)
     {
         VkPhysicalDeviceProperties deviceProperties = { };
@@ -55,6 +54,56 @@ int main(int argc, char** argv)
         queueFamilyProperties.resize(queueFamilyPropertyCount);
         vkGetPhysicalDeviceQueueFamilyProperties(m_physicalDevices[deviceIndex], &queueFamilyPropertyCount, &queueFamilyProperties[0]);
     }
+    */
+
+    // The above commented-out code runs through each physical device that the computer has.  For now, we just want to use the first one.
+    VkPhysicalDeviceProperties deviceProperties = { };
+    vkGetPhysicalDeviceProperties(m_physicalDevices[0], &deviceProperties);
+    cout << deviceProperties.deviceName << endl;
+
+    VkPhysicalDeviceFeatures deviceFeatures = { };
+    vkGetPhysicalDeviceFeatures(m_physicalDevices[0], &deviceFeatures);
+
+    VkPhysicalDeviceFeatures requiredFeatures = { };
+    requiredFeatures.multiDrawIndirect = deviceFeatures.multiDrawIndirect;
+    requiredFeatures.tessellationShader = VK_TRUE;
+    requiredFeatures.geometryShader = VK_TRUE;
+
+    VkPhysicalDeviceMemoryProperties memoryProperties;
+    vkGetPhysicalDeviceMemoryProperties(m_physicalDevices[0], &memoryProperties);
+
+    uint32_t queueFamilyPropertyCount = 0;
+    vector<VkQueueFamilyProperties> queueFamilyProperties;
+    vkGetPhysicalDeviceQueueFamilyProperties(m_physicalDevices[0], &queueFamilyPropertyCount, nullptr);
+    queueFamilyProperties.resize(queueFamilyPropertyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(m_physicalDevices[0], &queueFamilyPropertyCount, &queueFamilyProperties[0]);
+
+    // Create a logical device.
+    const VkDeviceQueueCreateInfo deviceQueueCreateInfo = {
+        VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, // sType
+        nullptr, // pNext
+        0,  // flags
+        0,  // queueFamilyIndex
+        1,  // queueCount
+        nullptr // pQueuePriorities
+    };
+
+    const VkDeviceCreateInfo deviceCreateInfo = {
+        VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO, // sType
+        nullptr, // pNext
+        0, // flags
+        1, // queueCreateInfoCount
+        &deviceQueueCreateInfo, // pQueueCreateInfos
+        0, // enabledLayerCount
+        nullptr, // ppEnabledLayerNames
+        0, // enabledExtensionCount
+        nullptr, // ppEnabledExtensionNames
+        &requiredFeatures // pEnabledFeatures
+    };
+
+    VkDevice m_logicalDevice;
+    result = vkCreateDevice(m_physicalDevices[0], &deviceCreateInfo, nullptr, &m_logicalDevice);
+    if (result != VK_SUCCESS) return 3;
     
     return 0;
 }
